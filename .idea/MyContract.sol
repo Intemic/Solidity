078861e06adc946f329ee21ef7afefc8546aaf4a
Anton <intemic@gmail.com>
2018-09-24 19:46:38 +0500

    Test

---
+++ .idea/MyContract.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.4.0;
+
+import  "openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol";
+
+contract MyCoin is ERC20Mintable {
+
+   string public constant name = "Blockchain Valley";
+
+   string public constant symbol = "BVA";
+
+   uint8 public constant decimals = 18;
+
+}
+
+
+contract MyICO {
+  uint public constant MAX_COIN =
+
+}
