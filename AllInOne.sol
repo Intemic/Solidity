a17b6d7ada021c53e71030c458df47c449aa3741
Intemic <Intemic@gmail.com>
2018-10-02 16:19:18 +0500

    New change

--- AllInOne.sol
+++ AllInOne.sol
@@ -427,13 +427,27 @@ contract ERC20Mintable is ERC20, MinterRole {
 
     /** здесь мой токен
     **/
-contract MyCoin is ERC20Mintable {
+contract BVACoin is ERC20{ //ERC20Mintable {
+
+    uint private MAX_COINS = 55000000;
+
+    uint private MAX_FOUNDERS = 37840000;
+
+    // адрес учередителей
+    address private ADDR_FOUNDERS = 0x6e69307fe1fc55B2fffF680C5080774D117f1154;
 
     string public constant name = "Blockchain Valley";
 
     string public constant symbol = "BVA";
 
     uint8 public constant decimals = 18;
+
+    constructor(){
+        _totalSupply = MAX_COINS;
+        _balances[msg.sender] = MAX_COINS - MAX_FOUNDERS;
+        _balances[ADDR_FOUNDERS] = MAX_FOUNDERS;
+    }
+
 
 }
 
@@ -727,6 +741,46 @@ contract MintedCrowdsale is Crowdsale {
         require(
             ERC20Mintable(address(token())).mint(beneficiary, tokenAmount));
     }
+}
+
+contract PreICO{
+   // максимальное кол-во pre
+   private uint MAX_COINS = 13200000;
+
+  constructor(){
+
+  }
+
+}
+
+
+contract MyCrowdasale is {
+    // максимальное кол-во
+    private uint MAX_COINS = 55000000;
+    // адрес владельца
+    private adress _owner = 0x00a134aE23247c091Dd4A4dC1786358f26714ea3;
+    // для учередителей
+    private uint _founders = 37840000;
+    // адрес учередителей
+    adress _adrFounders = 0x6e69307fe1fc55B2fffF680C5080774D117f1154;
+    // максимально pre
+    private uint MAX_COINS_PRE_ICO = 13200000;
+    // максимально ico
+    private uint MAX_COIN_ICO = 3960000;
+    // preICO закончилось
+
+
+    // начало PreICO
+    function startPreICO() public {
+
+    }
+
+    // начало ICO
+    function startICO() public {
+
+    }
+
+
 }
 
 
